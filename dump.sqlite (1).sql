8cfcd2dbaa0d76882c4c5571e57621755a0b5c3c
thaistressada <159055013+thaistressada@users.noreply.github.com>
2024-08-12 13:54:45 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE CAMPO(
+  ID INT(4),
+  CIDADE VARCHAR(30) UNIQUE,
+  CAPACIDADE INT(4)NOT NULL
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
